5ce4b6543b073ad1ab122c2eef35dbf5d5c922fe
phamthuat <phamthanhthuat2006@gmail.com>
2024-11-25 09:41:54 +0700

    Signed-off-by: phamthuat <phamthanhthuat2006@gmail.com>

---
+++ yeuthich.js
@@ -0,0 +1,8 @@
+function toggleLike(button) {
+    button.classList.toggle('liked');
+    if (button.classList.book-list('liked')) {
+        alert('Bạn đã bấm tim!');
+    } else {
+        alert('Bạn đã bỏ tim!');
+    }
+}
